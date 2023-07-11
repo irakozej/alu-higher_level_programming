50cfe7f60edb2b79a6322015c878a5f180ec1ede
irakozej <jeaniakoze68@gmail.com>
2023-07-11 01:53:27 -0700

    nn

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,7 +1,2 @@
-#!/usr/bin/python3
-<<<<<<< HEAD
-print(f"Programming is like building a multilingual puzzle")
-=======
-msg = '"Programming is like building a multilingual puzzle"
-print(msg)
->>>>>>> a593c4451f8ee92d430bb1dfac050a369fd64263
+#!/usr/bin/python3 
+print(f'"Programming is like building a multilingual puzzle')
