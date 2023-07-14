dd2d648b7b6c683e2e1fccd5dc3691e19e467629
irakozej <jeaniakoze68@gmail.com>
2023-07-14 05:14:00 -0700

    output

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-def element_at(my_list, idx, n):
-if idx < 0 or idx >= len(my_list):
-    retun my_list
-    my_list[idx] = n 
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    my_list[idx] = element
     return my_list
