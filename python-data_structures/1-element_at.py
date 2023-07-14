2ac69aec8779867ff3763935e94f7b9998a30cda
irakozej <jeaniakoze68@gmail.com>
2023-07-14 03:59:42 -0700

    output correction

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-def 1-element_at.py(my_list=[]):
-    for num in my_list
-    print(my_list[0])
+def 1-element_at.py(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+    retun none
+    return (my_list[idx])
+    
