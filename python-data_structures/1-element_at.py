f03aac84b7cc1ae224ff454914de229d4062df0e
irakozej <jeaniakoze68@gmail.com>
2023-07-14 04:33:54 -0700

    output

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def 1-element_at.py(my_list, idx):
     if idx < 0 or idx >= len(my_list):
-    retun none
-    return (my_list[idx])
-    
+    retun None
+    return my_list[idx)
