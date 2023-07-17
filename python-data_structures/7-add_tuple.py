06a7a0a4d3b354de4b4fc3df44a9a83d8ba3f40a
irakozej <jeaniakoze68@gmail.com>
2023-07-17 13:28:00 -0700

    worked o outputs

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+# 7-add_tuple.py
+
+
 def add_tuple(tuple_a=(), tuple_b=()):
- if len(tuple_a) < 2:
+    """Add two tuples."""
+    if len(tuple_a) < 2:
         if len(tuple_a) == 0:
             tuple_a = 0, 0
         else:
@@ -11,4 +15,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
         else:
             tuple_b = tuple_b[0], 0
 
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+# 9-max_integer.py
+
+
 def max_integer(my_list=[]):
-if len(my_list) == 0:
+    """Find the biggest integer of a list."""
+    if len(my_list) == 0:
         return (None)
 
     big = my_list[0]
