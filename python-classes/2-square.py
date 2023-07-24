86cdccedd323b0e9e7c4fdeadad31c8c333676f1
irakozej <jeaniakoze68@gmail.com>
2023-07-24 06:42:30 -0700

    2-file

---
+++ python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""5this module defines int variable"""
+
+class Square:
+    """this is class boddy"""
+    def __init__(self, size=0):
+        """decralation and  initializing class variables
+
+
+Args:
+            size (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+            """
+            if not isinstance(size, int):
+                RaiseTypeError("size must be an integer")
+
+                if size < 0:
+                    RaiseTypeError("size ust be positive")
+                    self.__size = size
