9aa2b6e100850fe95b9fa555428cbf81cde4e4cc
irakozej <jeaniakoze68@gmail.com>
2023-07-24 08:51:17 -0700

    never easy

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -5,8 +5,7 @@ class Square:
     """This is square class"""
 
     def __init__(self, size=0):
-
-         """
+        """
         Initializes a new instance of the Square class.
 
         Args:
@@ -16,14 +15,12 @@ class Square:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
         """
-
         if not isinstance(size, int):
-            Raise TypeError("size has to be of int type")
+            raise TypeError("size has to be of int type")
+        elif size < 0:
+            raise ValueError("size must be greater than or equal to 0")
+        self.__size = size
 
-            elif size < 0:
-                Raise ValueError("size must be greater than 0")
-             self.__size=size
-
-                def area(self):
-              """Return area of square"""
-              return (self.__size*self.__size)
+    def area(self):
+        """Return area of square"""
+        return self.__size * self.__size
