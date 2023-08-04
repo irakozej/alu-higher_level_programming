7db13207a8942320952d240a106238deb7e978db
irakozej <jeaniakoze68@gmail.com>
2023-08-04 17:00:56 +0000

    output

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
+
+
 """
-Module to fetch and display the status from https://alu-intranet.hbtn.io/status
+
+Module to fetch and display the status from given URLs
 """
 
 import urllib.request
 
-if __name__ == "__main__":
-    url = 'https://alu-intranet.hbtn.io/status'
-    
+def fetch_and_display_status(url):
+    """
+    Fetches the status from the given URL and displays the response body.
+    Args:
+        url (str): The URL to fetch the status from.
+    """
     with urllib.request.urlopen(url) as response:
         content = response.read()
         content = content.decode('utf-8')
-    
+
     print("Body response:")
     print("\t- type: {}".format(type(content)))
     print("\t- content: {}".format(content))
+
+if __name__ == "__main__":
+    intranet_url = 'https://intranet.hbtn.io/status'
+    local_url = 'http://0.0.0.0:5050/status'
+    
+    fetch_and_display_status(intranet_url)
+    fetch_and_display_status(local_url)
+
