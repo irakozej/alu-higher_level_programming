8099353918297abf4884f578fb722e3bd3e6b03f
irakozej <jeaniakoze68@gmail.com>
2023-09-30 21:22:59 +0000

    spaces gamn

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 
 const length = process.argv.length - 2;
 if (length === 0) {
-console.log('No argument');
+  console.log('No argument');
 } else if (length === 1) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
