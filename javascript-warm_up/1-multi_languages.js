048906147b070c518b3d6e79246e6bfa73fe9bee
Irakoze Jean Paul <jeaniakoze68@gmail.com>
2023-09-30 11:21:34 +0200

    not executable

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+console.log("C is fun");
+console.log("JavaScript is amazing");
+console.log("Python is cool");
---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const length = process.argv.length - 2;
+if (length==0){
+console.log("No argument");
+}
+else if (length===1)
+{
+    console.log("Argument found");
+}
+else {
+    console.log("Argument found")
+}
