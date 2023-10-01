e05819dccb27bb8d3682abe0546c9cb562ef71a0
irakozej <jeaniakoze68@gmail.com>
2023-10-01 09:29:28 +0000

    all-files

---
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+//this function returns number of occurrences in one list
+
+exports.nbOccurences = function (list, searchElement) {
+  return list.filter((value) => (value === searchElement)).length;
+};
---
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+//this function returns the inverse of a list
+
+exports.esrever = function (list) {
+  const rlist = [];
+  while (list.length > 0) {
+    rlist.push(list.pop());
+  }
+  return rlist;
+};
